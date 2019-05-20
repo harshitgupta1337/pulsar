7ded77a52fe1e67da7fe52e5dc5ac7b65da74944
Ubuntu <tyler@pulsar05.n4ugukvyz2xedmjjwyokk2nj3h.bx.internal.cloudapp.net>
2019-05-20 22:59:44 +0000

    Integrate java-serf-client with pulsar-common

--- java-serf-client/provision.sh
+++
@@ -1,6 +0,0 @@
-wget -q https://releases.hashicorp.com/serf/0.8.1/serf_0.8.1_linux_amd64.zip
-sudo apt-get -yq install unzip
-unzip serf_0.8.1_linux_amd64.zip
-sudo mv serf /usr/local/bin/
-sudo cp serf-agent.conf /etc/init/
-sudo start serf-agent
