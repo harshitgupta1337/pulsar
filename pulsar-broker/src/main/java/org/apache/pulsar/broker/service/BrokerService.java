fe4fdaad0bb2acb6567a7f41a0d84e406cc6707c
Tyler Landle <tlandle3@gatech.edu>
2019-06-15 23:08:15 +0000

    Updating unload to remove bundle

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
@@ -983,6 +983,8 @@ public class BrokerService implements Closeable, ZooKeeperCacheListener<Policies
                 closeFutures.add(topicFuture
                         .thenCompose(t -> t.isPresent() ? t.get().close() : CompletableFuture.completedFuture(null)));
             }
+            log.info("Removing bundle: {}", serviceUnit.toString());
+            pulsar.getCetusBrokerData().getBundleNetworkCoordinates().remove(serviceUnit.toString());
         });
         CompletableFuture<Void> aggregator = FutureUtil.waitForAll(closeFutures);
         aggregator.thenAccept(res -> result.complete(closeFutures.size())).exceptionally(ex -> {
